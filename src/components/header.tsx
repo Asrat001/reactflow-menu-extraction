bfe6f48acdf8f53d253b208d73a837853b066b05
assrat007 <asratadane169@gmail.com>
2024-04-19 14:37:43 +0300

    header updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,15 +26,13 @@ export function Header() {
 
 	return (
 		<header className="py-2 px-4 shadow-sm flex justify-between gap-3 flex-wrap">
-			<div className="flex gap-2 items-center">
+			<div className="flex    gap-2 items-center">
 				<Workflow size={24} />
-				<h1 className="text-xl font-bold text-center sm:text-2xl">
+				<h1 className="text-xl font-bold text-center sm:text-2xl ">
 				Menu Extractor and Visualization by by Asrat
 				</h1>
 			</div>
-			<Button size="sm" onClick={handleSaveClick}>
-				Save Changes 
-			</Button>
+			
 		</header>
 	)
 }

b1416ddbed5d573bf89ec6ef8a2d377ff297282e
assrat007 <asratadane169@gmail.com>
2024-04-19 15:24:01 +0300

    finaly updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { Workflow } from 'lucide-react'
+import { GitFork, Github, Workflow } from 'lucide-react'
 
 
 export function Header() {
@@ -9,11 +9,16 @@ export function Header() {
 
 	return (
 		<header className="py-2 px-4 shadow-sm flex justify-between gap-3 flex-wrap">
-			<div className="flex    gap-2 items-center">
-				<Workflow size={24} />
+			<div className="flex    gap-6 i items-center">
+			
+			<Workflow size={24} />
 				<h1 className="text-xl font-bold text-center sm:text-2xl ">
 				Menu Extractor and Visualization by by Asrat
 				</h1>
+			
+				<a href='https://github.com/Asrat001/reactflow-menu-extraction' target='_blank'>
+				<Github size={24}/>
+				</a>
 			</div>
 			
 		</header>
